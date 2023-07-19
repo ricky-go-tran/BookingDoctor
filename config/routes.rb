7e036c889a60b80992f128d6284c55b64733fcf3
Ricky <ricky.tran.goldenowl@gmail.com>
2023-07-19 10:29:32 +0700

    Add: ui for change password and change all to scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'report/index'
+    get 'report/show'
+    get 'report/create'
+    get 'report/new'
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :admin do
+    resources :reports
+  end
 end
